86ff001fc8cbf48da0d6f463907e2f365fb06185
menty44 <menty44@gmail.com>
2020-06-03 19:08:56 +0300

    added more empty activities

---
+++ app/src/main/java/com/blaqueyard/code/veges/ResetpassActivity.java
@@ -0,0 +1,14 @@
+package com.blaqueyard.code.veges;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResetpassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resetpass);
+    }
+}
