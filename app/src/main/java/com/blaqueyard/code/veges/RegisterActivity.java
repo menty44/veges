1a14702e1373d6391412745e52f0e8af2b0e27c7
menty44 <menty44@gmail.com>
2020-06-04 14:04:14 +0300

    added a lib for doing http requests

--- app/src/main/java/com/blaqueyard/code/veges/RegisterActivity.java
+++ app/src/main/java/com/blaqueyard/code/veges/RegisterActivity.java
@@ -13,6 +13,12 @@ import com.android.volley.RequestQueue;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+
+import org.json.JSONArray;
 
 import java.io.IOException;
 
@@ -28,6 +34,42 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final Context context = getApplicationContext();
+
+        AndroidNetworking.initialize(getApplicationContext());
+
+        AndroidNetworking.get("http://192.168.0.12:8080/roles")
+                .setPriority(Priority.IMMEDIATE)
+                .build()
+                .getAsJSONArray(new JSONArrayRequestListener() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+
+                        // do anything with response
+                        Log.d("net available ", (String.valueOf(response)));
+
+
+                        CharSequence text = String.valueOf(response);
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+                    @Override
+                    public void onError(ANError error) {
+                        // handle error
+                        Log.d("net available ", (String.valueOf(error)));
+
+
+                        CharSequence text = String.valueOf(error);
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+                });
 
     }
 
